8ef36559ada1dcffb23685fd8f58366a32e3b71d
RagunathS95 <149080983+RagunathS95@users.noreply.github.com>
2026-01-07 15:46:06 +0530

    add routers

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Props_Parent from './Props_Parent.jsx';
+import {Route, Routes} from 'react-router-dom';
+import Head from './Head.jsx';
 
 function App() {
   
-
   return (
     <>
-      <Props_Parent />
+    <Head />
+      
+        <Routes>
+          <Route path="/Prop-Parent" element={<Props_Parent />} />
+        </Routes>
+      
     </>
   )
 }
---
+++ src/Head.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Head(){
+    return(
+        <div>
+            <NavLink to="/Prop-Parent">Props Parent Component</NavLink>
+        </div>
+    )
+}
+export default Head;
